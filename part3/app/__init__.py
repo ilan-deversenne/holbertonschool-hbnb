808bdb1e8b4ccaef3b8da8425f92a806fd910355
Ilan <ilan.deversenne@holbertonstudents.com>
2026-03-10 14:32:15 +0100

    fix: Moved import in the function

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
 
-from app.api.v1.amenities import api as amenities_ns
-from app.api.v1.reviews import api as reviews_ns
-from app.api.v1.places import api as places_ns
-from app.api.v1.users import api as users_ns
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_restx import Api
@@ -14,6 +10,11 @@ db = SQLAlchemy()
 bcrypt = Bcrypt()
 
 def create_app(config_class=config.DevelopmentConfig):
+    from app.api.v1.amenities import api as amenities_ns
+    from app.api.v1.reviews import api as reviews_ns
+    from app.api.v1.places import api as places_ns
+    from app.api.v1.users import api as users_ns
+
     app = Flask(__name__)
     app.config.from_object(config_class)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
