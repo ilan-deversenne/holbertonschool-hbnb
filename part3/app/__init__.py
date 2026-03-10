7d78538037bdf0c0d0727acd9d65907257aba629
Ilan <ilan.deversenne@holbertonstudents.com>
2026-03-10 14:27:13 +0100

    Added SQLAlchemy & Bcrypt

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -4,13 +4,22 @@ from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.users import api as users_ns
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 from flask_restx import Api
 from flask import Flask
+import config
 
-def create_app(config_class="config.DevelopmentConfig"):
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+
+def create_app(config_class=config.DevelopmentConfig):
     app = Flask(__name__)
     app.config.from_object(config_class)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
+
+    db.init_app(app)
+    bcrypt.init_app(app)
 
     # Placeholder for API namespaces (endpoints will be added later)
     # Additional namespaces for places, reviews, and amenities will be added later
