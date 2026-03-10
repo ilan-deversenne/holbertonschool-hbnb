5fe4745459b94f3923a53b77e43f38d8dbc92fc6
aragoza <charlet.elliot@gmail.com>
2026-03-10 20:03:33 +0100

    append the protected decorator jwt to verify the token of the user on the page

--- part3/app/api/v1/amenities.py
+++ part3/app/api/v1/amenities.py
@@ -1,5 +1,6 @@
 from flask_restx import Namespace, Resource, fields
 from app.services import facade
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 api = Namespace('amenities', description='Amenity operations')
 
@@ -14,9 +15,15 @@ class AmenityList(Resource):
     @api.expect(amenity_model)
     @api.response(201, 'Amenity successfully created')
     @api.response(400, 'Invalid input data')
+    @api.response(401, 'Unauthorized user')
+    @jwt_required()
     def post(self):
         """Register a new amenity"""
         amenity_data = api.payload
+
+        actual_user = get_jwt_identity()
+        if not actual_user:
+            return {'error': 'Unauthorized user'}, 401
 
         def test(pair):
             key, value = pair
@@ -30,8 +37,14 @@ class AmenityList(Resource):
         return {'id': new_amenity.id, 'name': new_amenity.name}, 201
 
     @api.response(200, 'List of amenities retrieved successfully')
+    @api.response(401, 'Unauthorized user')
+    @jwt_required()
     def get(self):
         """Retrieve a list of all amenities"""
+        actual_user = get_jwt_identity()
+        if not actual_user:
+            return {'error': 'Unauthorized user'}, 401
+
         amenities = facade.get_all_amenities()
         list_amenities = [{
                 "id": amenity.id,
@@ -45,8 +58,14 @@ class AmenityList(Resource):
 class AmenityResource(Resource):
     @api.response(200, 'Amenity details retrieved successfully')
     @api.response(404, 'Amenity not found')
+    @api.response(401, 'Unauthorized user')
+    @jwt_required()
     def get(self, amenity_id):
         """Get amenity details by ID"""
+        actual_user = get_jwt_identity()
+        if not actual_user:
+            return {'error': 'Unauthorized user'}, 401
+
         try:
             amenity = facade.get_amenity(amenity_id)
         except Exception as e:
@@ -59,9 +78,14 @@ class AmenityResource(Resource):
     @api.response(200, 'Amenity updated successfully')
     @api.response(404, 'Amenity not found')
     @api.response(400, 'Invalid input data')
+    @api.response(401, 'Unauthorized user')
+    @jwt_required()
     def put(self, amenity_id):
         """Update an amenity's information"""
         amenity_data = api.payload
+        actual_user = get_jwt_identity()
+        if not actual_user:
+            return {'error': 'Unauthorized user'}, 401
 
         def test(pair):
             key, value = pair
