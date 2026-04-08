eec0e8940aa17254bf131c54af4753d08a5b7adc
elliot <charlet.elliot@gmail.com>
2026-04-08 14:55:01 +0200

    append the description and the price to the return of the GET all places

--- part3/app/api/v1/places.py
+++ part3/app/api/v1/places.py
@@ -66,6 +66,8 @@ class PlaceList(Resource):
         return [{
             'id': place.id,
             'title': place.title,
+            'description': place.description
+            'price': place.price
             'latitude': place.latitude,
             'longitude': place.longitude,
             'created_at': int(datetime.timestamp(place.created_at)),
